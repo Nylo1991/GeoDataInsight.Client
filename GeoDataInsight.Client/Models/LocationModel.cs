91bf83f93c7d62a9b96d6289fd8328d3ea539157
Nylo1991 <nylo.dk@gmail.com>
2026-05-07 19:49:12 -0300

    Atualização dos atributos da classe LocationModel

--- GeoDataInsight.Client/Models/LocationModel.cs
+++ GeoDataInsight.Client/Models/LocationModel.cs
@@ -1,16 +1,73 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace GeoDataInsight.Client.Models
 {
-    public class LocationModel
+    public class LocationModel : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Logradouro { get; set; } = string.Empty;
-        public string Numero { get; set; } = string.Empty;
-        public string Bairro { get; set; } = string.Empty;
-        public string Cep { get; set; } = string.Empty;
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public DateTime Timestamp { get; set; }
+        private int id;
+        private string logradouro = string.Empty;
+        private string numero = string.Empty;
+        private string bairro = string.Empty;
+        private string cep = string.Empty;
+        private double latitude;
+        private double longitude;
+        private DateTime timestamp;
+
+        public int Id
+        {
+            get => id;
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        public string Logradouro
+        {
+            get => logradouro;
+            set { logradouro = value; OnPropertyChanged(); }
+        }
+
+        public string Numero
+        {
+            get => numero;
+            set { numero = value; OnPropertyChanged(); }
+        }
+
+        public string Bairro
+        {
+            get => bairro;
+            set { bairro = value; OnPropertyChanged(); }
+        }
+
+        public string Cep
+        {
+            get => cep;
+            set { cep = value; OnPropertyChanged(); }
+        }
+
+        public double Latitude
+        {
+            get => latitude;
+            set { latitude = value; OnPropertyChanged(); }
+        }
+
+        public double Longitude
+        {
+            get => longitude;
+            set { longitude = value; OnPropertyChanged(); }
+        }
+
+        public DateTime Timestamp
+        {
+            get => timestamp;
+            set { timestamp = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
