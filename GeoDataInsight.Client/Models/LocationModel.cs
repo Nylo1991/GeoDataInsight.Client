daa0a0c5e358d5d6b725bb66f5ffa118cc63ab04
vncsqxy <0001078112@senaimgaluno.com.br>
2026-05-10 21:48:48 -0300

    feat(model): implementar INotifyPropertyChanged e IsSelected no LocationModel

--- GeoDataInsight.Client/Models/LocationModel.cs
+++ GeoDataInsight.Client/Models/LocationModel.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using Newtonsoft.Json;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace GeoDataInsight.Client.Models
 {
-    public class LocationModel
+    // AVISO IMPORTANTE: Faltava o ": INotifyPropertyChanged" aqui!
+    public class LocationModel : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Logradouro { get; set; } = string.Empty;
@@ -13,5 +16,24 @@ namespace GeoDataInsight.Client.Models
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public DateTime Timestamp { get; set; }
+        public string Key { get; set; }
+        public string Nome { get; set; }
+
+
+        private bool _isSelected;
+
+        [JsonIgnore]
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set { _isSelected = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
