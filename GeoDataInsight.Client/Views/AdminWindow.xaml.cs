f1ddaba7f5f1a9b34da9f04f9d1dfb911bc92230
vncsqxy <0001078112@senaimgaluno.com.br>
2026-05-10 21:50:06 -0300

    refactor(view): vincular AdminViewModel ao DataContext da AdminWindow

---
+++ GeoDataInsight.Client/Views/AdminWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GeoDataInsight.Client.ViewModels;
+
+namespace GeoDataInsight.Client.Views
+{
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+
+            // Isso conecta a View ao ViewModel! Sem isso, a tela fica vazia.
+            this.DataContext = new AdminViewModel();
+        }
+    }
+}
